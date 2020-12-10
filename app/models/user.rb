d2c685519c0ab76c852b7b7670db20d6f77bf23b
yoshimu708 <naoyasoccer0928@gmail.com>
2020-12-10 11:30:08 +0900

    room_userの中間テーブル作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :room_users
+  has_many :rooms, through: :room_users
 end
