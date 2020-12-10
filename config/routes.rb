f16de2a0d93c99945d321eb59ff5d6261ad865b5
yoshimu708 <naoyasoccer0928@gmail.com>
2020-12-10 11:26:35 +0900

    ルーティング・コントローラーの編集

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "rooms#index"
   resources :users, only:[:edit,:update]
+  resources :rooms, only: [:new, :create]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
