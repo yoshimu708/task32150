291b4cc809d9961386fe8b36e060d6302b15ae13
yoshimu708 <naoyasoccer0928@gmail.com>
2020-12-10 12:08:32 +0900

    メッセージ送信機能実装

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,6 @@
 class Room < ApplicationRecord
   has_many :room_users
   has_many :users, through: :room_users
+  has_many :messages
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "rooms#index"
   resources :users, only:[:edit,:update]
-  resources :rooms, only: [:new, :create]
-
+  resources :rooms, only: [:new, :create] do
+    resources :messages, only: [:index, :create]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
