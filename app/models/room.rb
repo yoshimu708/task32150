9a96b53f7b090fc3d4e130b10ae96033884fe4a6
yoshimu708 <naoyasoccer0928@gmail.com>
2020-12-10 17:21:30 +0900

    チャット機能実装

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,6 +1,8 @@
 class Room < ApplicationRecord
-  has_many :room_users
-  has_many :users, through: :room_users
-  has_many :messages
+  has_many :room_users, dependent: :destroy
+  has_many :users, through: :room_users  
+  has_many :messages, dependent: :destroy
+
+  validates :name, presence: true
 
 end
