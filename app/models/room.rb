11770ca9bc1a540a375c77e0ee16939277ca47c3
yoshimu708 <naoyasoccer0928@gmail.com>
2020-12-11 20:19:42 +0900

    room消去によるタスクの消去

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,9 +2,7 @@ class Room < ApplicationRecord
   has_many :room_users, dependent: :destroy
   has_many :users, through: :room_users  
   has_many :messages, dependent: :destroy
-  has_many :tasks
-
+  has_many :tasks, dependent: :destroy
 
   validates :name, presence: true
-
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,9 @@
 class Task < ApplicationRecord
   belongs_to :room
   belongs_to :user
+
+  validates :task_name, presence: true
+  validates :status,    presence: true
+  validates :limit,     presence: true
+
 end
