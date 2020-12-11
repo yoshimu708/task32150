058740d90fd3b446c63526c4c057bdcfe13c2580
yoshimu708 <naoyasoccer0928@gmail.com>
2020-12-11 18:04:49 +0900

    アソシエーション・ルーティングの作成

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,6 +2,8 @@ class Room < ApplicationRecord
   has_many :room_users, dependent: :destroy
   has_many :users, through: :room_users  
   has_many :messages, dependent: :destroy
+  has_many :tasks
+
 
   validates :name, presence: true
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,5 @@ class User < ApplicationRecord
   has_many :room_users
   has_many :rooms, through: :room_users
   has_many :messages
-
+  has_many :tasks
 end
