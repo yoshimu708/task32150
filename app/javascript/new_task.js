1270239240ad090f643efcbfecb9adbb94c550bb
yoshimu708 <naoyasoccer0928@gmail.com>
2020-12-13 18:58:52 +0900

    j Queryの導入とnewtaskページ

---
+++ app/javascript/new_task.js
@@ -0,0 +1,5 @@
+$(function(){
+  $("#task-create-btn").click(function(){
+    $("#new-task-create").fadeIn();
+  });
+});
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,14 @@
 const { environment } = require('@rails/webpacker')
 
+//ここから
+const webpack = require('webpack')
+environment.plugins.prepend('Provide',
+    new webpack.ProvidePlugin({
+        $: 'jquery/src/jquery',
+        jQuery: 'jquery/src/jquery'
+    })
+)
+//ここまで
+
+
 module.exports = environment
