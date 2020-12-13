9189423e2ff19035694effcda8ac46030d172e13
yoshimu708 <naoyasoccer0928@gmail.com>
2020-12-13 19:45:15 +0900

    タスク投稿のモーダル作成

--- app/javascript/new_task.js
+++ app/javascript/new_task.js
@@ -2,4 +2,10 @@ $(function(){
   $("#task-create-btn").click(function(){
     $("#new-task-create").fadeIn();
   });
+
+  $(".close-btn").click(function(){
+    $("#task-create-btn").fadeOut();
+    $("#new-task-create").fadeOut();
+  });
 });
+
