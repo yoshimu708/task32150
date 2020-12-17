4ed4c9cad8eff2dea3a46c240cd95e5f5fdb6b7b
yoshimu708 <naoyasoccer0928@gmail.com>
2020-12-17 16:45:25 +0900

    日本語テストメッセージの導入

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,14 +1,28 @@
 require 'rails_helper'
 RSpec.describe User, type: :model do
+  before do
+    @user = FactoryBot.build(:user)
+  end
   describe 'ユーザー新規登録' do
     it "nicknameが空だと登録できない" do
-      user =FactoryBot.build(:user)
-      user.nickname = ""
-      user.valid?
-      binding.pry
-      expect(user.errors.full_messages).to include "Nickname can't be blank"
+      @user.nickname = ""
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Nicknameを入力してください"
     end
     it "emailが空では登録できない" do
+      @user.email= ""
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Eメールを入力してください"
+    end
+    it "passwordが空では登録できない" do
+      @user.password= ""
+      @user.valid?
+      expect(@user.errors.full_messages).to include "パスワードを入力してください", "パスワード（確認用）とパスワードの入力が一致しません"
+    end
+    it "password_confirmationが空では登録できない" do
+      @user.password_confirmation= ""
+      @user.valid?
+      expect(@user.errors.full_messages).to include "パスワード（確認用）とパスワードの入力が一致しません"
     end
   end
 end
