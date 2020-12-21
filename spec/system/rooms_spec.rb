3fc56e7a691895cced3c24b3740c1e5661ca9324
yoshimu708 <naoyasoccer0928@gmail.com>
2020-12-21 11:32:11 +0900

    メッセージの結合テストコード

--- spec/system/rooms_spec.rb
+++ spec/system/rooms_spec.rb
@@ -9,16 +9,16 @@ RSpec.describe "チャットルームの削除機能", type: :system do
     # サインインする
     sign_in(@room_user.user)
 
-    # 作成されたチャットルームへ遷移する
+    # 作成されたチャットルームへ遷移
     click_on(@room_user.room.name)
 
-    # メッセージ情報を5つDBに追加する
-    FactoryBot.create_list(:message, 5, room_id: @room_user.room.id, user_id: @room_user.user.id)
+    # メッセージ情報を3つDBに追加
+    FactoryBot.create_list(:message, 3, room_id: @room_user.room.id, user_id: @room_user.user.id)
 
-    # 「チャットを終了する」ボタンをクリックすることで、作成した5つのメッセージが削除されていることを確認する
+    # 「チャットを終了する」ボタンをクリックすることで、作成した3つのメッセージが削除されていることを確認する
     expect {
       find_link("チャットを終了する",  href: room_path(@room_user.room)).click
-    }.to change { @room_user.room.messages.count }.by(-5)
+    }.to change { @room_user.room.messages.count }.by(-3)
 
     # トップページに遷移していることを確認する
     expect(current_path).to eq root_path
